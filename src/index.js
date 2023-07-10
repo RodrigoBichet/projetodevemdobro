67aac63f833ad526df23f055be7612320f542491
Rodrigo Bichet <rodrigobichet.pl010@academico.ifsul.edu.br>
2023-07-10 14:34:50 -0300

    third commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// const itemCiclope = document.getElementById("ciclope");
+// const itemjeanGrey = document.getElementById("jean-grey");
+
+const personagens = document.querySelectorAll(".personagem");
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener("mouseenter", () => {
+        //para celular
+        if (window.innerHeight < 450) {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+
+        removerSelecaoDoPersonagem();
+        personagem.classList.add("selecionado");
+
+        alterarImagemPersonagemSelecionado(personagem);
+
+        alterarNomePersonagemSelecionado(personagem);
+
+        alterarDescricaoPersonagemSelecionado(personagem);
+    });
+});
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById("descricao-personagem");
+    descricaoPersonagem.innerText = personagem.getAttribute("data-description");
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById("nome-personagem");
+    nomePersonagem.innerText = personagem.getAttribute("data-nome");
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector(".personagem-grande");
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `../public/ASSETS/img/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector(".selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
